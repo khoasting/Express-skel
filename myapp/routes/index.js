b3eec7f9223cee0fbfe04107b2127bd352f7f620
khoatdd <trantandangkhoa@vienthonga.com>
2016-11-08 10:39:09 +0700

    add mongoose module

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,29 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
-var app = express();
+var router = express.Router();
 
-app.set('view engine', 'jade');
-app.set('views', './views');
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(upload.array()); // for parsing multipart/form-data
-app.use(express.static('public'));
-
-app.get('/', function(req, res){
-  res.send("recieved your request!");
-});
-
-app.post('/', function(req, res){
-  console.log(req.body);
-  res.send("recieved your request!");
-});
-
-/*/!* GET home page. *!/
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});*/
+});
 
-module.exports = app;
+module.exports = router;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,22 +1,16 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
+
 var app = express();
 
 app.set('view engine', 'jade');
 app.set('views', './views');
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(upload.array()); // for parsing multipart/form-data
-app.use(express.static('public'));
-
 app.get('/', function(req, res){
-  res.send("recieved your request!");
+  res.render("addcustomer");
+  
 });
 
-app.post('/users', function(req, res){
+app.post('/', function(req, res){
   console.log(req.body);
   res.send("recieved your request!");
 });
