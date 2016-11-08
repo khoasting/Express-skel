93558655fc8cb01e2863d4eff338c1a3d946fe49
khoatdd <trantandangkhoa@vienthonga.com>
2016-11-08 10:06:58 +0700

    add nodemon module

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,21 +1,10 @@
 var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-router.get('/add-customer', function(req, res, next) {
-  res.send('respond with a resource');
-});
-module.exports = router;
-var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
 var app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -23,9 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(upload.array()); // for parsing multipart/form-data
 app.use(express.static('public'));
 
-app.post('/', function(req, res){
+app.get('/', function(req, res){
+  res.send("recieved your request!");
+});
+
+app.post('/users', function(req, res){
   console.log(req.body);
   res.send("recieved your request!");
 });
 
-app.listen(3000);
+/* GET users listing. */
+
+module.exports = app;
